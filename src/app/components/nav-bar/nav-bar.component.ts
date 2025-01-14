5137143db5b8a713d2cf1db54a203d8937681d57
nika <nikalobjanidze014@gmail.com>
2025-01-14 16:43:20 +0400

    Nav Bar Done

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,41 +1,41 @@
-import { Component } from '@angular/core';
-//
+import { Component, HostListener } from '@angular/core';
 import { Router,} from '@angular/router';
-//
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  lastScroll = 0;
 
   constructor(private router: Router) {}
+  /// ROUTER FOR NAV-BAR LINKS   /// ROUTER FOR NAV-BAR LINKS
   isActive(route: string): boolean {
     return this.router.url === route || (route === '/' && this.router.url === '/');
   }
+  /// ROUTER FOR NAV-BAR LINKS   /// ROUTER FOR NAV-BAR LINKS
 
+  /// SCROLL FUNCTION ON NAV BAR   /// SCROLL FUNCTION ON NAV BAR
+  @HostListener('window:scroll', ['$event'])
+  onScroll(): void {
+    let currentScroll = window.pageYOffset;
+    let navbar = document.querySelector('#nav-bar') as HTMLElement;
+
+    if (navbar) {
+      if (currentScroll > this.lastScroll && !navbar.classList.contains('scroll-down')) {
+        navbar.classList.remove('scroll-up');
+        navbar.classList.add('scroll-down');
+      }
+      else if (currentScroll < this.lastScroll && navbar.classList.contains('scroll-down')) {
+        navbar.classList.remove('scroll-down');
+        setTimeout(() => {
+          navbar.classList.add('scroll-up');
+        });  
+      }
+      this.lastScroll = currentScroll;
+    }
+  }
+    /// SCROLL FUNCTION ON NAV BAR   /// SCROLL FUNCTION ON NAV BAR
 }
   
-//   let body = document.body;
-// let lastScroll = 0;
-
-// window.addEventListener("scroll", () => {
-//   let currentScroll = window.pageYOffset;
-//   if (currentScroll <= 0) {
-//     body.classList.remove("scroll-up");
-//     return;
-//   }
-
-//   if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-//     body.classList.remove("scroll-up");
-//     body.classList.add("scroll-down");
-//   } else if (
-//     currentScroll < lastScroll &&
-//     body.classList.contains("scroll-down")
-//   ) {
-//     body.classList.remove("scroll-down");
-//     body.classList.add("scroll-up");
-//   }
-//   lastScroll = currentScroll;
-// });
 
