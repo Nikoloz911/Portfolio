701dc3de28986e6c1f7cf69eba28bffb5e40f58a
nika <nikalobjanidze014@gmail.com>
2025-03-01 15:21:30 +0400

    Makeing Javascript Page. Done Description

--- src/app/components/languages/javascript/javascript.component.ts
+++ src/app/components/languages/javascript/javascript.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { ImageModalComponent } from '../../image-modal/image-modal.component';
 @Component({
   selector: 'app-javascript',
   templateUrl: './javascript.component.html',
   styleUrl: './javascript.component.scss'
 })
 export class JavascriptComponent {
-
+@ViewChild('imageModal') imageModal: ImageModalComponent | undefined;
+  openImage(imageSrc: string): void {
+    if (this.imageModal) {
+      this.imageModal.open(imageSrc);  
+    }
+  }
 }
