0a361d7ae111d6349b10465d32c4746832dc64f2
Nikoloz911 <nikalobjanidze014@gmail.com>
2025-03-30 17:12:01 +0400

    Added API .

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from '../../services/portfolio.service';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit {
+  summary: string = '';
 
+  constructor(private portfolioService: PortfolioService) {}
+  async ngOnInit() {
+    this.summary = await this.portfolioService.getSummary();
+  }
 }
